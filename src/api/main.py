d6d14f51d5a55c0160f1bdb75c21619be7cc6696
kevin <332976194@qq.com>
2025-02-22 15:52:25 +0800

    init fastapi

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,28 @@
+import tomllib
+import uvicorn
 from fastapi import FastAPI
+from routers import users, items
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, FastAPI!"}
+app.include_router(users.user_router, prefix="/users")
+app.include_router(items.item_router, prefix="/items")
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello, FastAPI!"}
+
+if __name__ == "__main__":
+    with open("config.toml", "rb") as f:
+        config = tomllib.load(f)
+
+    server_config = config["server"]
+
+    uvicorn.run(
+        app,
+        host=server_config["host"],
+        port=server_config["port"],
+        workers=server_config.get("workers"), # get方法防止key不存在报错
+        log_level=server_config.get("log_level"), # get方法防止key不存在报错
+        reload=server_config.get("reload") # get方法防止key不存在报错
+    )
